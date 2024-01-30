72e216938e2c48711172db49deccf509f60d15b6
BenitaNivedhithaJ <benita.nivedhitha@gmail.com>
2024-01-30 16:33:13 +0530

    webhelp

---
+++ Getting Started Guide for Facebook using Oxygen XML/Output/webhelp-responsive/oxygen-webhelp/app/context-help/context-help-map.js
@@ -0,0 +1,1 @@
+var contextHelpMap = [{"appname":"", "appid":"concept_em2_d3m_tzb", "path":"Get_Started_Intorduction.html"},{"appname":"", "appid":"task_pkq_q3m_tzb", "path":"Create_a_Facebook_account.html"},{"appname":"", "appid":"setup_account", "path":"setup_account.html"}];
---
+++ Getting Started Guide for Facebook using Oxygen XML/Output/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,10 @@
+var keywords=[{w:"Create",p:["p0"]},{w:"a",p:["p0"]},{w:"Facebook",p:["p0"]},{w:"account",p:["p0","p3"]},{w:"Get",p:["p1"]},{w:"Started",p:["p1"]},{w:"Context",p:["p2"]},{w:"Sensitive",p:["p2"]},{w:"Help",p:["p2"]},{w:"Set",p:["p3"]},{w:"up",p:["p3"]},{w:"your",p:["p3"]}];
+var ph={};
+ph["p0"]=[0, 1, 2, 3];
+ph["p1"]=[4, 5];
+ph["p2"]=[6, 7, 8];
+ph["p3"]=[9, 10, 11, 3];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
